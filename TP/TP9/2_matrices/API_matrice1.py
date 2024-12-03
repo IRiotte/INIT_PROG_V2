4ba453d33172ca0cbfeadc9d99b35a2601105dcf
IRiotte <ilane.riotte@etu.univ-orleans.fr>
2024-12-03 18:27:33 +0100

    TP9

--- TP/TP9/2_matrices/API_matrice1.py
+++ TP/TP9/2_matrices/API_matrice1.py
@@ -162,3 +162,41 @@ def sauve_matrice(la_matrice, nom_fichier):
             cpt += 1
             if cpt == la_matrice[2]:
                 matrice_writer.writerow(ligne)
+
+
+def get_ligne(matrice, ligne):
+
+    liste_ligne = []
+    longueur_colonne = get_nb_colonnes(matrice)
+    for i in range(longueur_colonne*ligne, longueur_colonne*(ligne+1)):
+        liste_ligne.append(matrice[2][i])
+    return liste_ligne
+
+def get_colonne(matrice, colonne):
+
+    liste_col = []
+    nb_col = get_nb_colonnes(matrice)
+    nb_ligne = get_nb_lignes(matrice)
+    for i in range(0+colonne, nb_col*nb_ligne, nb_col):
+        liste_col.append(matrice[2][i])
+    return liste_col
+
+def get_diagonale_principale(matrice):
+
+    ...
+
+def get_diagonale_secondaire(matrice):
+
+    ...
+
+def transpose(matrice):
+
+    ...
+
+def is_triangulaire_inf(matrice):
+
+    ...
+
+def bloc(matrice, ligne, colonne, hauteur, largeur):
+    
+    ...
